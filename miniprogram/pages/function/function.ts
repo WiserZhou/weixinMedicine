18fa9d441c6980e8684b7c2bdf03ac6cde2187eb
WiserZhou <wiserx@qq.com>
2023-09-19 14:51:39 +0800

    'definite'

--- miniprogram/pages/function/function.ts
+++ miniprogram/pages/function/function.ts
@@ -5,14 +5,148 @@ Page({
    * 页面的初始数据
    */
   data: {
+    resultArray: [],
+    resultList: [],
+    showPage: false  // 控制是否显示页面的标志位
+  },
+  takePhoto: function () {
+    var that = this; // 保存当前页面引用
 
+    wx.chooseMedia({
+      count: 1,
+      mediaType: ['image', 'video'],
+      sourceType: ['camera', 'album'],
+      success: function (res) {
+        const tempMediaPath = res.tempFiles[0].tempFilePath;
+        wx.getFileSystemManager().readFile({
+          filePath: tempMediaPath,
+          encoding: 'base64',
+          success: function (res) {
+            const base64Data = res.data;
+            const imageUrl = base64Data;
+            const topNum = 5;
+            const API_KEY = "1LsIPkOmeSi0ZNQr6WNegCRM";
+            const SECRET_KEY = "fxLFHNGOBEjbZFW2A3LNkCAv1im8Z3ha";
+            const apiUrl = "https://aip.baidubce.com/rpc/2.0/ai_custom/v1/classification/selection";
+
+            wx.request({
+              url: 'https://aip.baidubce.com/oauth/2.0/token?grant_type=client_credentials&client_id=' + API_KEY + '&client_secret=' + SECRET_KEY,
+              method: "POST",
+              header: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+              },
+
+              success: function (res) {
+                // 获取访问令牌成功
+                const accessToken = res.data.access_token;
+                wx.request({
+                  url: apiUrl + "?access_token=" + accessToken,
+                  method: "POST",
+                  header: {
+                    "Content-Type": "application/json"
+                  },
+                  data: {
+                    image: imageUrl,
+                    top_num: topNum
+                  },
+
+                  success: (res) => {
+                    function transferString(string: string): string {
+                      if (string === 'bcc') {
+                        return "基底细胞癌";
+                      } else if (string === 'mel') {
+                        return "黑色素瘤";
+                      } else if (string === 'nv') {
+                        return "黑素细胞痣（正常）";
+                      }
+                      return ''
+                    }
+                    // 请求成功的处理逻辑
+                    console.log(res.data);
+                    const resultArray = res.data.results;
+                    console.log(resultArray.length)
+                    for (let i = 0; i < resultArray.length; i++) {
+                      resultArray[i].name = transferString(resultArray[i].name)
+                      resultArray[i].score = resultArray[i].score * 100
+                      resultArray[i].score = resultArray[i].score.toFixed(5)
+                    }
+                    var currentDate = new Date();
+                    console.log(currentDate);
+                    let item = { name: '', score: '', date: '' };
+                    item.date = currentDate.toLocaleDateString() + '   ' + currentDate.toLocaleTimeString();
+                    resultArray.push(item);
+
+                    that.data.resultList.unshift(resultArray);
+                    wx.setStorage({
+                      key: resultArray[3].date,
+                      data: resultArray,
+                      success: function () {
+                        console.log('数据保存成功');
+                      }
+                    });
+                    that.setData({
+                      resultData: resultArray,
+                      resultList: that.data.resultList
+                    });
+                    console.log(that.data.resultList)
+                  },
+                  fail: function (res) {
+                    // 请求失败的处理逻辑
+                    console.error(res);
+                  }
+                });
+              },
+              fail: function (res) {
+                // 获取访问令牌失败
+                console.error(res);
+              }
+            })
+          },
+          fail: function (res) {
+            console.error(res);
+          }
+        });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {
-
+  onLoad: function () {
+    // wx.clearStorage({
+    //   success: function(res) {
+    //     console.log("本地数据已清空");
+    //   }
+    // });
+    var that = this;
+    wx.getStorageInfo({
+      success: function (res) {
+        var keys = res.keys;
+        for (var i = 0; i < keys.length; i++) {
+          var key = keys[i];
+          if (key.startsWith("202")) {
+            console.log('key:::' + key);
+            var data = wx.getStorageSync(key);
+            console.log("key: " + key + ", data: " + data);
+            that.data.resultList.push(data);
+          }
+        }
+        console.log(that.data.resultList); // 输出结果数组
+        that.setData({
+          resultList: that.data.resultList,
+          showPage: true
+        });
+      }
+    });
+  },
+  showInfo: function (event) {
+    console.log(event);
+    var index = event.currentTarget.dataset.index;
+    wx.navigateTo({
+      url: '/pages/function/info/info?index=' + index + '&' + 'date=' + this.data.resultList[index][3].date
+    });
   },
 
   /**
@@ -47,7 +181,26 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-
+    var that = this;
+    wx.getStorageInfo({
+      success: function (res) {
+        var keys = res.keys;
+        for (var i = 0; i < keys.length; i++) {
+          var key = keys[i];
+          if (key.startsWith("202")) {
+            console.log('key:::' + key);
+            var data = wx.getStorageSync(key);
+            console.log("key: " + key + ", data: " + data);
+            that.data.resultList.push(data);
+          }
+        }
+        console.log(that.data.resultList); // 输出结果数组
+        that.setData({
+          resultList: that.data.resultList,
+          showPage: true
+        });
+      }
+    });
   },
 
   /**
---
+++ miniprogram/pages/function/info/info.ts
@@ -0,0 +1,272 @@
+// pages/function/info/info.ts
+import * as echarts from '../../../components/ec-canvas/echarts';
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    time: [],
+    ecBar: {
+      onInit: function (canvas, width, height, dpr) {
+        const barChart = echarts.init(canvas, null, {
+          width: width,
+          height: height,
+          devicePixelRatio: dpr // new
+        });
+        canvas.setChart(barChart);
+        barChart.setOption(getBarOption());
+
+        return barChart;
+      }
+    },
+
+    ecScatter: {
+      onInit: function (canvas, width, height, dpr) {
+        const scatterChart = echarts.init(canvas, null, {
+          width: width,
+          height: height,
+          devicePixelRatio: dpr // new
+        });
+        canvas.setChart(scatterChart);
+        scatterChart.setOption(getScatterOption());
+
+        return scatterChart;
+      }
+    }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+    var that = this;
+    // 异步获取本地缓存中的数据
+    wx.getStorage({
+      key: '' + this.options.date,
+      success: function (res) {
+        var data = res.data;
+        console.log(data); // 输出：value
+        that.setData({
+          time: data
+        })
+        //console.log(that.data.time)
+      }
+    });
+
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
+
+function getBarOption() {
+  return {
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+        type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+      }
+    },
+    legend: {
+      data: ['热度', '正面', '负面']
+    },
+    grid: {
+      left: 20,
+      right: 20,
+      bottom: 15,
+      top: 40,
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'value',
+        axisLine: {
+          lineStyle: {
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          color: '#666'
+        }
+      }
+    ],
+    yAxis: [
+      {
+        type: 'category',
+        axisTick: { show: false },
+        data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+        axisLine: {
+          lineStyle: {
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          color: '#666'
+        }
+      }
+    ],
+    series: [
+      {
+        name: '热度',
+        type: 'bar',
+        label: {
+          normal: {
+            show: true,
+            position: 'inside'
+          }
+        },
+        data: [300, 270, 340, 344, 300, 320, 310]
+      },
+      {
+        name: '正面',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          normal: {
+            show: true
+          }
+        },
+        data: [120, 102, 141, 174, 190, 250, 220]
+      },
+      {
+        name: '负面',
+        type: 'bar',
+        stack: '总量',
+        label: {
+          normal: {
+            show: true,
+            position: 'left'
+          }
+        },
+        data: [-20, -32, -21, -34, -90, -130, -110]
+      }
+    ]
+  };
+}
+
+function getScatterOption() {
+
+  var data = [];
+  var data2 = [];
+
+  for (var i = 0; i < 10; i++) {
+    data.push(
+      [
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 40)
+      ]
+    );
+    data2.push(
+      [
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100),
+        Math.round(Math.random() * 100)
+      ]
+    );
+  }
+
+  var axisCommon = {
+    axisLabel: {
+      textStyle: {
+        color: '#C8C8C8'
+      }
+    },
+    axisTick: {
+      lineStyle: {
+        color: '#fff'
+      }
+    },
+    axisLine: {
+      lineStyle: {
+        color: '#C8C8C8'
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: '#C8C8C8',
+        type: 'solid'
+      }
+    }
+  };
+
+  return {
+    backgroundColor: '#eee',
+    xAxis: axisCommon,
+    yAxis: axisCommon,
+    legend: {
+      data: ['aaaa', 'bbbb']
+    },
+    visualMap: {
+      show: false,
+      max: 100,
+      inRange: {
+        symbolSize: [20, 70]
+      }
+    },
+    series: [{
+      type: 'scatter',
+      name: 'aaaa',
+      data: data
+    },
+    {
+      name: 'bbbb',
+      type: 'scatter',
+      data: data2
+    }
+    ],
+    animationDelay: function (idx) {
+      return idx * 50;
+    },
+    animationEasing: 'elasticOut'
+  };
+}
